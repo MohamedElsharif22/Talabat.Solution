a65936656d9d03d56f14175983681e73c51c1617
Mohmed Kamal <mohamed.elsharif022@gmail.com>
2024-10-29 16:58:28 +0300

    Add project files.

---
+++ Talabat.Core/Entities/Brand.cs
@@ -0,0 +1,8 @@
+﻿namespace Talabat.Core.Entities
+{
+    public class Brand : BaseEntity
+    {
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; } = new HashSet<Product>();
+    }
+}
---
+++ Talabat.Core/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Talabat.Core.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PictureUrl { get; set; }
+        public decimal Price { get; set; }
+        public Category Category { get; set; }
+        public int CategoryId { get; set; }
+        public Brand Brand { get; set; }
+        public int BrandId { get; set; }
+    }
+}
